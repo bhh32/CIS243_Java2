6adcd0da6d7945acee054fed80c1487390dbd911
Bryan Hyland <bryan.hyland32@gmail.com>
2019-04-08 17:21:55 -0700

    Module 1 Homework Passed All Instructor Tests

--- Module1/Homework/AlphabetFrequency.java
+++ Module1/Homework/AlphabetFrequency.java
@@ -2,143 +2,50 @@ import java.util.Arrays;
 
 public class AlphabetFrequency {
    private static final int ALPHABET_COUNT = 26;
+   private static final int NOT_FOUND = 0;
    
    private String data;
    private int[] alphabet = new int[ALPHABET_COUNT];
    private int size;
    
-   public AlphabetFrequency(String data) {
-      this.data = data.toLowerCase();
-      sortData();
-      
-      for (int i = 0; i < this.data.length(); ++i) {
-         if(Character.isLetter(this.data.charAt(i))) {
-            switch(data.charAt(i)) {
-               case 'a':
-               case 'A':
-                  alphabet[0]++;
-               break;
-               case 'b':
-               case 'B':
-                  alphabet[1]++;
-               break;
-               case 'c':
-               case 'C':
-                  alphabet[2]++;
-               break;
-               case 'd':
-               case 'D':
-                  alphabet[3]++;
-               break;
-               case 'e':
-               case 'E':
-                  alphabet[4]++;
-               break;
-               case 'f':
-               case 'F':
-                  alphabet[5]++;
-               break;
-               case 'g':
-               case 'G':
-                  alphabet[6]++;
-               break;
-               case 'h':
-               case 'H':
-                  alphabet[7]++;
-               break;
-               case 'i':
-               case 'I':
-                  alphabet[8]++;
-               break;
-               case 'j':
-               case 'J':
-                  alphabet[9]++;
-               break;
-               case 'k':
-               case 'K':
-                  alphabet[10]++;
-               break;
-               case 'l':
-               case 'L':
-                  alphabet[11]++;
-               break;
-               case 'm':
-               case 'M':
-                  alphabet[12]++;
-               break;
-               case 'n':
-               case 'N':
-                  alphabet[13]++;
-               break;
-               case 'o':
-               case 'O':
-                  alphabet[14]++;
-               break;
-               case 'p':
-               case 'P':
-                  alphabet[15]++;
-               break;
-               case 'q':
-               case 'Q':
-                  alphabet[16]++;
-               break;
-               case 'r':
-               case 'R':
-                  alphabet[17]++;
-               break;
-               case 's':
-               case 'S':
-                  alphabet[18]++;
-               break;
-               case 't':
-               case 'T':
-                  alphabet[19]++;
-               break;
-               case 'u':
-               case 'U':
-                  alphabet[20]++;
-               break;
-               case 'v':
-               case 'V':
-                  alphabet[21]++;
-               break;
-               case 'w':
-               case 'W':
-                  alphabet[22]++;
-               break;
-               case 'x':
-               case 'X':
-                  alphabet[23]++;
-               break;
-               case 'y':
-               case 'Y':
-                  alphabet[24]++;
-               break;
-               case 'z':
-               case 'Z':
-                  alphabet[25]++;
-               break;
+   public AlphabetFrequency(String data) {      
+      if (data != null) {
+         this.data = data.toLowerCase();
+         sortData();
+         
+         int idx = 0;
+         int counter = 0;
+         if (!this.data.isEmpty()) {
+            while (counter < this.data.length()) {               
+               idx = this.data.charAt(counter) - 'a';
+               alphabet[idx]++;
+               counter++;               
             }
-         }
+         }         
+         calculateSize();
       }
-      
-      calculateSize();
    }
    
    public int get(char letter) {
       // Make sure that the parameter is a letter      
-      if(!Character.isLetter(letter)) {
+      if (!Character.isLetter(letter)) {
          throw new IllegalArgumentException("Parameter passed into get() was not a letter!");
       }
+      
       letter = Character.toLowerCase(letter);
       int idx = 0;
-      for (int i = 0; i < data.length(); ++i) {
-         if (data.charAt(i) == letter) {   
-            idx = data.charAt(i) - 'a';
+      int counter = 0;
+      while (counter < data.length()) {
+         if (data.charAt(counter) == letter) {  
+            idx = (data.charAt(counter) - 'a');
+            return alphabet[idx];
+         }
+         else {
+            counter++;
          }
       }
       
-      return alphabet[idx];
+      return NOT_FOUND;
    }
    
    public void set(char letter, int value) {
@@ -146,10 +53,36 @@ public class AlphabetFrequency {
          throw new IllegalArgumentException("You passed in a negative value or a non-letter character to set()!");
       }
       
-      int idx = letter - 'a';
+      char lowerCaseLetter = Character.toLowerCase(letter);
+      alphabet[lowerCaseLetter - 'a'] = value;
       
-      alphabet[idx] = value;
+      if (value != 0) {
+         if (data == null) {
+               char[] temp = new char[value];
+               for (int i = 0; i < temp.length; ++i) {
+                  temp[i] = lowerCaseLetter;
+               }
+               
+               data = new String(temp);
+         }
+         else {
+            for (int i = 0; i < value; ++i) {         
+               data += lowerCaseLetter;
+            }
+         }
+      }
+      else {
+         char[] temp = new char[data.length()];
+         for (int j = 0; j < data.length(); ++j) {
+            if (data.charAt(j) != lowerCaseLetter) {
+               temp[j] = data.charAt(j);
+            }
+         }
+         
+         data = new String(temp);
+      }
       
+      sortData();      
       calculateSize();
    }
    
@@ -167,11 +100,16 @@ public class AlphabetFrequency {
    public String toString() {
       String temp = "[";
       
-      for (int i = 0; i < data.length() - 1; ++i) {
-         temp += data.charAt(i);
+      if (data.length() > 0) {
+         for (int i = 0; i < data.length() - 1; ++i) {
+            temp += data.charAt(i);
+         }
+         
+         temp += data.charAt(data.length() - 1) + "]";      
       }
-      
-      temp += data.charAt(data.length() - 1) + "]";      
+      else {
+         temp += "]";
+      }
       
       return temp;
    }
@@ -184,16 +122,8 @@ public class AlphabetFrequency {
    }
    
    public AlphabetFrequency minus(AlphabetFrequency other) {
-      AlphabetFrequency temp = new AlphabetFrequency("TempString");
-      
-      // Reset all counts to 0
-      for (int c : temp.alphabet) {
-         c = 0;
-      }
-      
-      // Reset size to 0
-      temp.size = 0;
-      
+      AlphabetFrequency temp = new AlphabetFrequency("");
+            
       // Subtract other.alphabet[idx] from this.alphabet[idx]
       for (int i = 0; i < ALPHABET_COUNT; ++i) {
          temp.alphabet[i] = this.alphabet[i] - other.alphabet[i];
@@ -204,9 +134,96 @@ public class AlphabetFrequency {
          }
       }
       
+      for (int i = 0; i < ALPHABET_COUNT; ++i) {
+         for (int j = 0; j < temp.alphabet[i]; ++j) {
+            switch(i) {
+               case 0:
+                  temp.data += 'a';
+                  break;
+               case 1:
+                  temp.data += 'b';
+                  break;
+               case 2:
+                  temp.data += 'c';
+                  break;
+               case 3:
+                  temp.data += 'd';
+                  break;
+               case 4:
+                  temp.data += 'e';
+                  break;
+               case 5:
+                  temp.data += 'f';
+                  break;
+               case 6:
+                  temp.data += 'g';
+                  break;
+               case 7:
+                  temp.data += 'h';
+                  break;
+               case 8:
+                  temp.data += 'i';
+                  break;
+               case 9:
+                  temp.data += 'j';
+                  break;
+               case 10:
+                  temp.data += 'k';
+                  break;
+               case 11:
+                  temp.data += 'l';
+                  break;
+               case 12:
+                  temp.data += 'm';
+                  break;
+               case 13:
+                  temp.data += 'n';
+                  break;
+               case 14:
+                  temp.data += 'o';
+                  break;
+               case 15:
+                  temp.data += 'p';
+                  break;
+               case 16:
+                  temp.data += 'q';
+                  break;
+               case 17:
+                  temp.data += 'r';
+                  break;
+               case 18:
+                  temp.data += 's';
+                  break;
+               case 19:
+                  temp.data += 't';
+                  break;
+               case 20:
+                  temp.data += 'u';
+                  break;
+               case 21:
+                  temp.data += 'v';
+                  break;
+               case 22:
+                  temp.data += 'w';
+                  break;
+               case 23:
+                  temp.data += 'x';
+                  break;
+               case 24:
+                  temp.data += 'y';
+                  break;
+               case 25:
+                  temp.data += 'z';
+                  break;
+            }
+         }
+      }
+      temp.sortData();
+      temp.calculateSize();
       return temp;
    }
    
+   // Utility Methods
    private void sortData() {
       
       String temp = " ";
@@ -229,18 +246,22 @@ public class AlphabetFrequency {
       data = temp;
    }
    
-   private static void calculateSize() {
-      for (int j : alphabet) {
-         size += j;
+   private void calculateSize() {
+      size = 0;
+      for (int i = 0; i < ALPHABET_COUNT; ++i) {
+         size += alphabet[i];
       }
    }
    
    // TEST
    public static void main(String[] args)
-   {
-      AlphabetFrequency af = new AlphabetFrequency("Bryan  12!");
-      System.out.println(af.size());
-      System.out.println(af);
-      System.out.println(af.get('B'));
+   {      
+      AlphabetFrequency af = new AlphabetFrequency("Bryan 23@!");
+      System.out.println(af.get('c'));
+      System.out.println("Before set size: " + af.size());
+      af.set('a', 5);
+      System.out.println("Data after set: " + af);
+      System.out.println("After set size: " + af.size());
+      
    }
 }
