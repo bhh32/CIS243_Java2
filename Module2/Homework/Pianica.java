40fb6e7efb0dcb3820210a9a5765327ac54caa44
Bryan Hyland <BryanHyland@student.olympic.edu>
2019-04-15 12:23:19 -0700

    Worked on Week2 Homework - Finished Pianica.hasKey(char key)

--- Module2/Homework/Pianica.java
+++ Module2/Homework/Pianica.java
@@ -8,7 +8,8 @@
 public class Pianica implements Piano {
    public static final String KEYS =
        "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";  // keyboard layout
-   private static final int CONCERT_A = 440;   
+   private static final int CONCERT_A = 440;
+   private static final int HIGHEST_PITCH_KEY_VALUE = KEYS.length() - 11;  
    private Note[] notes = new Note[37];
    
    public Pianica() {
@@ -22,15 +23,32 @@ public class Pianica implements Piano {
    } 
        
    public void playNote(int pitch) {
-      
+      /*if (pitch <= 0) {
+         playKey(KEYS[pitch + 24]);
+      }
+      else {
+         if (pitch > 0 && pitch < HIGHEST_PITCH_KEY_VALUE) {
+            playKey(KEYS[KEYS.size() - pitch])
+         }
+      }*/
    }
    
    public boolean hasKey(char key) {
+      for (int i = 0; i < KEYS.length(); ++i) {
+         if (KEYS.charAt(i) == key) {
+            return true;
+         }
+      }
       return false;
    }
    
    public void playKey(char key) {
-   
+      if (hasKey(key)) {
+         notes[KEYS.indexOf(key)].play();
+      }
+      else {
+         throw new IllegalArgumentException("An illegal key was input into playKey()!");
+      }
    }
    
    public double sample() {
@@ -47,5 +65,7 @@ public class Pianica implements Piano {
    
    public static void main(String[] args) {
       Pianica p = new Pianica();
+      System.out.println(p.hasKey('e'));
+      System.out.println(p.hasKey('3'));
    }
 }
